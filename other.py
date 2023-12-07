23ee175e4a1e6d4233f6d420df4544d153cf9387
KadenQMc <kadenmc018@gmail.com>
2023-12-06 23:13:22 -0500

    Added copy option

--- other.py
+++ other.py
@@ -4,13 +4,14 @@ import shutil
 from tqdm import tqdm
 
 
-def create_subset(dataset_folder, save_dir, ratio):
+def create_subset(dataset_folder, save_dir, ratio, op):
     """
     Create subset of the original data
     :param dataset_folder: folder with data to take subset of
     :param save_dir: directory to move subset to
     :param ratio: ratio of files to move from dataset_folder to save_dir
-    :return:
+    :param op: "copy" to copy the files or "move" to move the files
+    :return: nothing
     """
     subfolders = os.listdir(dataset_folder)
 
@@ -21,7 +22,7 @@ def create_subset(dataset_folder, save_dir, ratio):
     for folder in tqdm(subfolders):
         folder_path = os.path.join(dataset_folder, folder)
 
-        # shuffle files to random images from each class are chosen
+        # shuffle files so image selection is random
         files = os.listdir(folder_path)
         random.shuffle(files)
 
@@ -29,12 +30,18 @@ def create_subset(dataset_folder, save_dir, ratio):
         num = len(files)
         num = int(num * ratio)
 
+        # Move samples
         for i in range(0, num):
             source = os.path.join(dataset_folder, folder, files[i])
             dest = os.path.join(save_dir, folder, files[i])
-            shutil.move(source, dest)
+
+            if op == 'move':
+                shutil.move(source, dest)
+            elif op == 'copy':
+                shutil.copy(source, dest)
 
 
-create_subset(r"C:\Users\kaden\Main\CS678\final_project\ASL",
-              r"C:\Users\kaden\Main\CS678\final_project\0.1_test",
-              0.1)
+# create_subset(r"C:\Users\kaden\Main\CS678\final_project\sets\ASL",
+#               r"C:\Users\kaden\Main\CS678\final_project\sets\0.1_test",
+#               0.1, 'move')
+
