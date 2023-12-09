0e79af065cc5dfb71959a0324ea3fbffcbfeecf5
KadenQMc <kadenmc018@gmail.com>
2023-12-09 11:01:20 -0500

    increase standard number of classes

--- test.py
+++ test.py
@@ -87,7 +87,10 @@ if __name__ == '__main__':
             # Compute and save confusion matrix
             confusion_matrix = metrics.confusion_matrix(true_label, pred_label)
             cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=confusion_matrix,
-                                                        display_labels=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+                                                        display_labels=['0', '1', '2', '3', '4', '5', '6', '7', '8',
+                                                                        '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                                                        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                                                                        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
             cm_display.plot()
             plt.savefig(os.path.join(args.save_dir, f'confusion_matrix.jpg'))
 
